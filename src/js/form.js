9016245c9d6b32d01132649299bcec3c55ebdb01
Ivan Propadovic <i.propadovic@gmail.com>
2018-01-22 17:02:39 +0100

    validation form

--- src/js/form.js
+++ src/js/form.js
@@ -1,3 +1,24 @@
 $(document).ready(function(){
-  $("#form").validate();
+  $("#form").validate({});
+  // var first_name=$("#form").find("input#first_name");
+  // var last_name=$("#form").find("input#last_name");
+  // var button=$("#form").find("input#send");
+  // var inputs=$("#form input[type='text']");
+  //
+  // $.each(inputs,function(){
+  //   $(this).on("blur",function(){
+  //     if ($(this).val() != "") {
+  //       button.addClass("active");
+  //     }
+  //   });
+  // });
+  // if (first_name.hasClass("valid") && last_name.hasClass("valid")) {
+  //     button.addClass("active");
+  // }
 });
+
+function thankYou() {
+  if ($("#send").hasClass("active")) {
+    alert("Thank you!");
+  }
+}
