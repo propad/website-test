750406e7549bbecd27884d5118a4ef577c60a56f
Ivan Propadovic <i.propadovic@gmail.com>
2018-01-23 16:23:40 +0100

    thanks page

--- src/js/form.js
+++ src/js/form.js
@@ -1,6 +1,15 @@
 $(document).ready(function(){
 
-  var button=$("#form").find("input#send");
+  var button=$("#form").find("button#send");
+
+  function allFilled() {
+    var filled = true;
+    $('#form .required').each(function() {
+      if($(this).val() == '') filled = false;
+    });
+    return filled;
+  }
+
   button.on("click",function(){
 
     if (button.hasClass("active")) {
@@ -8,20 +17,11 @@ $(document).ready(function(){
       alert("Thank you!");
 
     }
-
   });
 
   $('#form .required').on('keyup', function() {
      if(allFilled()) button.addClass('active');
      else button.removeClass('active');
   });
-
-  function allFilled() {
-     var filled = true;
-     $('#form .required').each(function() {
-         if($(this).val() == '') filled = false;
-     });
-     return filled;
-  }
 
 });
