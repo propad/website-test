7fa77c82ab11f807c9633b8176ecd1b8ca640a8f
Ivan Propadovic <i.propadovic@gmail.com>
2018-01-23 11:57:09 +0100

    alert submit

--- build/js/main.js
+++ build/js/main.js
@@ -1,27 +1,30 @@
 $(document).ready(function(){
-  $("#form").validate({});
-  // var first_name=$("#form").find("input#first_name");
-  // var last_name=$("#form").find("input#last_name");
-  // var button=$("#form").find("input#send");
-  // var inputs=$("#form input[type='text']");
-  //
-  // $.each(inputs,function(){
-  //   $(this).on("blur",function(){
-  //     if ($(this).val() != "") {
-  //       button.addClass("active");
-  //     }
-  //   });
-  // });
-  // if (first_name.hasClass("valid") && last_name.hasClass("valid")) {
-  //     button.addClass("active");
-  // }
-});
 
-function thankYou() {
-  if ($("#send").hasClass("active")) {
-    alert("Thank you!");
+  var button=$("#form").find("input#send");
+  button.on("click",function(){
+
+    if (button.hasClass("active")) {
+
+      alert("Thank you!");
+
+    }
+
+  });
+
+  $('#form .required').on('keyup', function() {
+     if(allFilled()) button.addClass('active');
+     else button.removeClass('active');
+  });
+
+  function allFilled() {
+     var filled = true;
+     $('#form .required').each(function() {
+         if($(this).val() == '') filled = false;
+     });
+     return filled;
   }
-}
+
+});
 
 window.onscroll = scroll1;
 
